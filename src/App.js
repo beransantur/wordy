b6aaf8e9c73a3b6104444c8410b87cffd96e6158
Beran Santur <beransantur@gmail.com>
2022-07-02 23:36:03 +0200

    navbar and footer have been added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
+import * as Icon from "react-bootstrap-icons";
+
 function App() {
   return (
     <>
-      <div className="container">
+      <div className="wordle-container">
+        <section className="bg-dark nav-container">
+          <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+            <div class="container pt-2">
+              <a class="navbar-brand" href="#home">
+                <Icon.Quora size={35} />
+              </a>
+              <button
+                class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNavAltMarkup"
+              >
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto">
+                  <a class="nav-link" href="#home">
+                    HOME
+                  </a>
+                  <a class="nav-link" href="#user">
+                    USER
+                  </a>
+                  <a class="nav-link" href="#contact">
+                    CONTACT
+                  </a>
+                </div>
+              </div>
+            </div>
+          </nav>{" "}
+        </section>
         <div className="wordle">
           <div className="header">Wordle</div>
           <div className="board">
@@ -53,6 +85,11 @@ function App() {
             <button>Submit</button>
           </form>
         </div>
+        <section class="mt-3 p-2 bg-dark text-white text-center footer w-100">
+          <div class="container">
+            <p class="lead">Copyright &copy; 2022 Beran Santur</p>
+          </div>
+        </section>
       </div>
     </>
   );
